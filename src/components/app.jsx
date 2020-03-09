1b0c96177013152e145e0f3bbc38334123a05506
Benjamin de Boissieu <bendeb@hotmail.com>
2020-03-09 23:08:00 +0100

    with solution

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-
+import GoogleMapReact from 'google-map-react';
 import FlatList from './flat-list.jsx';
-import SimpleMap from './map.jsx';
+import Marker from './marker.jsx';
 import flats from '../../data/flats.js'
-
 
 class App extends Component {
   constructor(props){
@@ -14,16 +13,34 @@ class App extends Component {
     };
   }
 
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
   selectFlat = (index) => {
     this.setState({selectedFlat: flats[index]});
   }
+
+
 
   render(props){
     return (
       <div>
         <FlatList flats = {this.state.flats} selectedFlat = {this.state.selectedFlat} selectFlat = {this.selectFlat}/>
         <div className="map-container">
-          <SimpleMap selectedFlat = {this.state.selectedFlat}/>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
+            defaultCenter={this.center()}
+            defaultZoom={12}
+          >
+            <Marker
+              lat={this.state.selectedFlat.lat}
+              lng={this.state.selectedFlat.lng}
+            />
+          </GoogleMapReact>
         </div>
       </div>
     );
